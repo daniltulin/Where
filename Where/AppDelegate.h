3abd6c3d332039f132daea8233202b41856dbf19
tulindanil <tulindanil@gmail.com>
2016-11-25 21:14:06 +0200

    Initial Commit

---
+++ Where/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Where
+//
+//  Created by Danil Tulin on 11/25/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Where/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Where
+//
+//  Created by Danil Tulin on 11/25/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
