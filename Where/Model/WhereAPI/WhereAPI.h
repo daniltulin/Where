1aa1ec28af977235c7e6ec2c633cc7bb93a976f6
tulindanil <tulindanil@gmail.com>
2016-11-25 23:26:53 +0200

    Change file structure

---
+++ Where/Model/WhereAPI/WhereAPI.h
@@ -0,0 +1,13 @@
+//
+//  WhereAPI.h
+//  Where
+//
+//  Created by Danil Tulin on 11/25/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WhereAPI : NSObject
+
+@end
---
+++ Where/Model/WhereAPI/WhereAPI.m
@@ -0,0 +1,13 @@
+//
+//  WhereAPI.m
+//  Where
+//
+//  Created by Danil Tulin on 11/25/16.
+//  Copyright © 2016 Daniil Tulin. All rights reserved.
+//
+
+#import "WhereAPI.h"
+
+@implementation WhereAPI
+
+@end
