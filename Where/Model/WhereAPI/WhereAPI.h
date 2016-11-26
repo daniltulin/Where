3b8c77d118274c8afe1b2e1f568f8b5f29477f8b
tulindanil <tulindanil@gmail.com>
2016-11-26 10:20:05 +0200

    Add plug

--- Where/Model/WhereAPI/WhereAPI.h
+++ Where/Model/WhereAPI/WhereAPI.h
@@ -8,6 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
+@import MapKit;
+
+
+@interface Request : NSObject
+
+@property (nonatomic, copy, readonly) NSString *query;
+@property (nonatomic, readonly) NSUInteger rowsQty;
+@property (nonatomic, readonly) NSUInteger columnsQty;
+@property (nonatomic, readonly) MKCoordinateRegion region;
+
++ (instancetype)requestWithQuery:(NSString *)query
+                         rowsQty:(NSUInteger)rowsQty
+                      columnsQty:(NSUInteger)columnsQty
+                       andRegion:(MKCoordinateRegion)region;
+
+@end
+
+@interface Coloring : NSObject
+
+@property (nonatomic) NSArray<NSArray<NSNumber *> *> *matrix;
+
+@end
+
+typedef void (^ColoringHandler)(Coloring *);
+
 @interface WhereAPI : NSObject
+
++ (void)obtainColoring:(Request *)request
+           withHandler:(ColoringHandler)handler;
 
 @end
--- Where/Model/WhereAPI/WhereAPI.m
+++ Where/Model/WhereAPI/WhereAPI.m
@@ -8,6 +8,86 @@
 
 #import "WhereAPI.h"
 
+#include <math.h>
+
+@interface Request ()
+
+@property (nonatomic, copy, readwrite) NSString *query;
+@property (nonatomic, readwrite) NSUInteger rowsQty;
+@property (nonatomic, readwrite) NSUInteger columnsQty;
+@property (nonatomic, readwrite) MKCoordinateRegion region;
+
+@end
+
+@implementation Request
+
++ (instancetype)requestWithQuery:(NSString *)query
+                         rowsQty:(NSUInteger)rowsQty
+                      columnsQty:(NSUInteger)columnsQty
+                       andRegion:(MKCoordinateRegion)region {
+    Request *request = [[Request alloc] init];
+    request.query = [query copy];
+    request.rowsQty = rowsQty;
+    request.columnsQty = columnsQty;
+    request.region = region;
+    return request;
+}
+
+@end
+
+@interface Coloring ()
+
+@end
+
+@implementation Coloring
+
+@end
+
+bool in_circle(float x, float y, float a, float b) {
+    return pow(x / a, 2) + pow(y / b, 2) <= 1;
+}
+
+float z_centered(float x, float y, float a, float b) {
+    if (in_circle(x, y, a, b))
+        return sqrt(1 - (pow(x / a, 2) + pow(y / b, 2)));
+    else
+        return 0;
+}
+
+float z(float x, float y, float a, float b) {
+    return z_centered(x - a, y - b, a, b);
+}
+
 @implementation WhereAPI
+
++ (void)obtainColoring:(Request *)request
+           withHandler:(ColoringHandler)handler {
+    Coloring *coloring = [[Coloring alloc] init];
+
+    NSUInteger rowsQty = request.rowsQty, columnsQty = request.columnsQty;
+
+    NSMutableArray *rows = [NSMutableArray arrayWithCapacity:rowsQty];
+    for (int row = 0; row < rowsQty; row++) {
+        rows[row] = [NSMutableArray arrayWithCapacity:columnsQty];
+        for (int column = 0; column < columnsQty; column++) {
+            float z_ = z(row, column,
+                        columnsQty/2, rowsQty/2);
+            rows[row][column] = [NSNumber numberWithFloat:z_];
+            NSLog(@"%d %d %f", row, column, z_);
+        }
+    }
+
+    dispatch_block_t wrapper = ^{
+        if (handler != nil)
+            handler(coloring);
+    };
+    dispatch_block_t completion_block = ^{
+        dispatch_async(dispatch_get_main_queue(), wrapper);
+    };
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        sleep(1);
+        completion_block();
+    });
+}
 
 @end
